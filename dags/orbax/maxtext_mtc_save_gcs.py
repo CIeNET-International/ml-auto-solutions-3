22357339245b1d487291035298876ebfb1e07dab
Yuna Tzeng <yuna.tzeng@cienet.com>
2025-08-11 12:26:17 +0800

    Ref: Refactored validation_util.py file, into two classes ValidationReplicator and Validationpod.
    Add: Add DAG maxtext_mtc_orbax_sav02_save_gcs. (Test can save in Bucket using replicator.Multi-tier Checkpointing)
    
    Fix: Refactor cpc dict into a CheckpointConfiguration class, that holds relevant info about cpc metadata
    
    Fix: Change name of DAG to be mtc only.
    
    Fix: Fix some try and catch exceptions that seems to be unnecessary.
    
    Fix: Refactor whole Cpc delete task into two task initiate_cpc and wait_to_delete. wait_to_delete task is a sensor task that will check every 10 s if the delition already happened.
    
    Fix: Change name_id of the test to be related to Orbax. Create new tag with Orbax. Remove --restart-on-user-code-failure flag since is already outdated in both main and develop branch
    
    Fix: Backup interval to longer. Only testing local saves. GCS testing is in DAG002
    
    Change name  maxtext_multi_tier_emergency_checkpoint_save_gcs.py
    
    Add: Added DAG emergency Checkpointer sav01-gcs. Testing Replication in bucket
    
    Change name to sav02 to more clarity
    
    Fix: Fixed logic to get last bucket saved ce=heckpoint instead of first one

--- dags/orbax/maxtext_mtc_save_gcs.py
+++ dags/orbax/maxtext_mtc_save_gcs.py
@@ -7,47 +7,38 @@ with HNS (Hierarchical Namespace)
 import datetime
 from airflow import models
 from dataclasses import dataclass
+from typing import Optional
 
 from dags import composer_env, gcs_bucket
 from dags.common import test_owner
 from dags.common.vm_resource import DockerImage, XpkClusters
 from dags.multipod.configs import gke_config
 from dags.multipod.configs.common import SetupMode
-from dags.orbax.util import logging_mtc as log
-from dags.orbax.util import multi_tier_checkpoint_util as mtc
+from dags.orbax.util import validation_util
+from dags.orbax.util import checkpoint_util
 from xlml.utils.xpk import BRANCH_ABHINAV_MTC
 from xlml.utils.gke import zone_to_region
+from dags.orbax.util.validation_util import BaseValidation
 
 
 # Global variables across test configurations.
 SCHEDULE = "0 12 * * *" if composer_env.is_prod_env() else None
 DAG_TEST_NAME = "maxtext_mtc_orbax_save_gcs"
-BASE_OUTPUT_DIRECTORY = f"{gcs_bucket.MTC_AUTOMATION_BUCKET}/{DAG_TEST_NAME}"
+BASE_OUTPUT_DIR = gcs_bucket.MTC_AUTOMATION_BUCKET
+
+# Only one version of the Docker image is supported at the moment.
+# Other versions (e.g., "stable") may be introduced later.
 DOCKER_IMAGES = [(
     SetupMode.NIGHTLY,
-    DockerImage.MAXTEXT_TPU_JAX_NIGHTLY,
+    DockerImage.MAXTEXT_TPU_JAX_ORBAX_HEAD,
 )]
 RAM_DISK = "/local"
 USE_REPLICATOR = True
 
 
 @dataclass
-class Testconfig:
-  """Holds the general configuration for a checkpointing test.
-
-  Attributes:
-    cluster: The specified cluster to be used for the test.
-    machine_type: The type of machine (e.g., GPU, TPU).
-    accelerator: The type of accelerator (e.g., GPU, TPU) to use.
-    slices: The number of slices to be used.
-    model_name: The name of the model being tested.
-    short_id (str): A short id to be used for naming the test run.
-    replicator_min: The time the replicator takes to backup checkpoints to bucket.
-    step: The current step of the training process.
-    local_checkpoint_step: The step interval for local checkpoints.
-    checkpoint_step: The step interval for the checkpoints store in the bucket.
-    ram_disk_mi: The size about the RAM disk in the CSI driver, in Mi.
-  """
+class TestConfig:
+  """Holds the general configuration for a checkpointing test."""
 
   cluster: XpkClusters
   machine_type: str
@@ -55,11 +46,98 @@ class Testconfig:
   slices: list[int]
   model_name: str
   short_id: str
-  replicator_min: int
+  replicator_backup_time: int
   step: int
   local_checkpoint_step: int
   checkpoint_step: int
-  ram_disk_mi: str
+  ram_disk_size: str
+  cpc_config: checkpoint_util.CheckpointConfiguration
+
+
+  def __init__(
+      self,
+      cluster: XpkClusters,
+      machine_type: str,
+      accelerator: str,
+      slices: list[int],
+      model_name: str,
+      short_id: str,
+      replicator_backup_time: int,
+      step: int,
+      local_checkpoint_step: int,
+      ram_disk_size_in_mi: str,
+      checkpoint_step: Optional[int] = None,
+  ):
+    """
+    Initializes the test configurations.
+
+    Args:
+      cluster: The specified cluster to be used for the test.
+      machine_type: The type of machine (e.g., GPU, TPU).
+      accelerator: The type of accelerator (e.g., GPU, TPU) to use.
+      slices: The number of slices to be used.
+      model_name: The name of the model being tested.
+      short_id: A short identifier for the test run.
+      replicator_backup_time: The allowed time for replicator takes to backup
+        and store checkpoint to bucket
+      step: The current step of the training process.
+      local_checkpoint_step: The step interval for local checkpoints.
+      checkpoint_step: The step interval for the checkpoints store in the
+        bucket.
+      ram_disk_size_in_mi: The size in mebibytes (Mi) about the RAM disk in the
+        CSI driver. The unit is in mebibytes (Mi) but the value should be passed
+        as a string with the unit, e.g., "2G" or "2048M". Defaults to "100G"".
+    """
+
+    self.cluster = cluster
+    self.machine_type = machine_type
+    self.accelerator = accelerator
+    self.slices = slices
+    self.model_name = model_name
+    self.short_id = short_id
+    self.replicator_backup_time = replicator_backup_time
+    self.step = step
+    self.local_checkpoint_step = local_checkpoint_step
+    self.checkpoint_step = checkpoint_step
+    self.ram_disk_size = ram_disk_size_in_mi
+    self.cpc_config = checkpoint_util.CheckpointConfiguration(
+        project_id=self.cluster.project,
+        region=zone_to_region(self.cluster.zone),
+        cluster_name=self.cluster.name,
+        gcs_bucket=gcs_bucket.MTC_AUTOMATION_BUCKET.removeprefix("gs://"),
+        ramdisk_memory_in_mi=self.ram_disk_size,
+        machine_type=self.machine_type,
+    )
+
+  def generate_workload_command(
+      self,
+      checkpoint_dir: str,
+      out_folder: str,
+      slice_number: int,
+  ) -> str:
+    run_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+    run_name = f"{self.short_id}-mtc-{slice_number}x-{self.accelerator}-{run_time}"
+    return (
+        "export TPU_PREMAPPED_BUFFER_SIZE=52428800000 && "
+        "export TPU_PREMAPPED_BUFFER_TRANSFER_THRESHOLD_BYTES=52428800000 && "
+        "python3 -m MaxText.train MaxText/configs/base.yml "
+        "remat_policy=full "
+        "global_parameter_scale=1 "
+        f"base_output_directory={out_folder} "
+        "dataset_type=synthetic "
+        f"steps={self.step} "
+        "per_device_batch_size=1 "
+        "max_target_length=256 "
+        f"model_name={self.model_name} "
+        "per_device_batch_size=2 "
+        "reuse_example_batch=1 "
+        "enable_emergency_checkpoint=true "
+        f"local_checkpoint_directory={checkpoint_dir} "
+        f"local_checkpoint_period={self.local_checkpoint_step} "
+        f"use_replicator_service=true "
+        f"replicator_backup_interval_minutes={self.replicator_backup_time} "
+        f"run_name={run_name}",
+    )
 
 
 with models.DAG(
@@ -79,57 +157,36 @@ with models.DAG(
     concurrency=2,
 ) as dag:
   test_configs = [
-      Testconfig(
+      TestConfig(
           cluster=XpkClusters.TPU_V5P_128_CLUSTER_ORBAX,
           machine_type="ct5p-hightpu-4t",
           accelerator="v5p-128",
           slices=[2],
           model_name="llama2-7b",
-          short_id="max-sv",
-          replicator_min=30,
-          step=100,
+          short_id="max-sv-gcs",
+          replicator_backup_time=1,
+          step=200,
           local_checkpoint_step=20,
-          checkpoint_step=25,
-          ram_disk_mi="800000Mi",
+          checkpoint_step=15,
+          ram_disk_size_in_mi="800000Mi",
       ),
   ]
+  output_folder = f"maxtext_mtc_orbax_save_gcs"
   for mode, image in DOCKER_IMAGES:
     for test_config in test_configs:
-      cpc_conf = mtc.CheckpointConfiguration(
-          project_id=test_config.cluster.project,
-          region=zone_to_region(test_config.cluster.zone),
-          cluster_name=test_config.cluster.name,
-          gcs_bucket=gcs_bucket.MTC_AUTOMATION_BUCKET.removeprefix("gs://"),
-          ramdisk_memory_in_mi=test_config.ram_disk_mi,
-          machine_type=test_config.machine_type,
-      )
       for slice_num in test_config.slices:
         # We conditionally set the trigger_rule on the first task.
         # If first task group failed the next one can execute.
-        init_delete_cpc = mtc.initiate_cpc_deletion(cpc_conf)
-        wait_delete_cpc = mtc.wait_for_cpc_deletion.override(
+        wait_delete_cpc = checkpoint_util.wait_for_cpc_deletion.override(
             trigger_rule="all_done"
-        )(cpc_conf)
-        apply_cpc = mtc.apply_cpc(cpc_conf)
-        run_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
-        run_name = (
-            f"{test_config.short_id}-mtc-{slice_num}x-{test_config.accelerator}-{run_time}"
+        )(test_config.cpc_config)
+        apply_cpc = checkpoint_util.apply_cpc(test_config.cpc_config)
+        workload_command = test_config.generate_workload_command(
+            checkpoint_dir=RAM_DISK,
+            out_folder=output_folder,
+            slice_number=slice_num,
         )
-        workload_command = (
-            "export TPU_PREMAPPED_BUFFER_SIZE=52428800000 && "
-            "export TPU_PREMAPPED_BUFFER_TRANSFER_THRESHOLD_BYTES=52428800000 && "
-            "python3 -m MaxText.train MaxText/configs/base.yml remat_policy=full "
-            f"global_parameter_scale=1 base_output_directory={BASE_OUTPUT_DIRECTORY} "
-            f"dataset_type=synthetic steps={test_config.step} per_device_batch_size=1 "
-            f"max_target_length=256 model_name={test_config.model_name} per_device_batch_size=2 "
-            f"reuse_example_batch=1 enable_emergency_checkpoint=true checkpoint_period={test_config.checkpoint_step} "
-            f"local_checkpoint_directory={RAM_DISK} local_checkpoint_period={test_config.local_checkpoint_step} "
-            f"use_replicator_service={USE_REPLICATOR} replicator_backup_interval_minutes={test_config.replicator_min} "
-            f"run_name={run_name}",
-        )
-
-        start_time = log.generate_timestamp()
-
+        start_time = validation_util.generate_timestamp()
         maxtext_chkpt_run_test = gke_config.get_gke_config(
             num_slices=slice_num,
             cluster=test_config.cluster,
@@ -137,14 +194,13 @@ with models.DAG(
             test_name=f"{test_config.short_id}-mtc",
             run_model_cmds=workload_command,
             docker_image=image.value,
-            test_owner=test_owner.CAMILO,
+            test_owner=test_owner.CAMILO_Q,
         ).run(
             ramdisk_directory=RAM_DISK,
             mtc_enabled=True,
             xpk_branch=BRANCH_ABHINAV_MTC,
             skip_post_process=True,
         )
-
         cleanup_command = (f"rm -rf {RAM_DISK}/*",)
         ram_disk_cleanup = gke_config.get_gke_config(
             num_slices=slice_num,
@@ -153,7 +209,7 @@ with models.DAG(
             test_name=f"{test_config.short_id}-cleanup",
             run_model_cmds=cleanup_command,
             docker_image=image.value,
-            test_owner=test_owner.CAMILO,
+            test_owner=test_owner.CAMILO_Q,
         ).run(
             ramdisk_directory=RAM_DISK,
             mtc_enabled=True,
@@ -161,51 +217,49 @@ with models.DAG(
             skip_post_process=True,
         )
 
-        vali_step = test_config.step- 1
-        vali_step_list = [
-            i
-            for i in range(0, vali_step, test_config.local_checkpoint_step)
-        ]
-        vali_step_list.append(vali_step)
 
-        end_time = log.generate_timestamp()
+        end_time = validation_util.generate_timestamp()
 
         # Here we are looking for the string '(blocking + background)'.
         # We will compare expected steps with the ones we found when query this
         # regex. Should be the same
-        validate_steps = log.validate_log_with_step(
-            project_id=test_config.cluster.project,
-            location=zone_to_region(test_config.cluster.zone),
-            cluster_name=test_config.cluster.name,
-            text_filter="(blocking + background).",
-            start_time=start_time,
-            end_time=end_time,
-            vali_step_list=vali_step_list,
+        validation_with_default_pod_config = validation_util.factory_create_validation_config(
+            test_config=test_config,
+            namespace="default",
+            text_filter="(blocking + background)."
+        )
+
+        # Actual Task that would check if local stored steps
+        # Need to pass start_time and end_time as variables here since there is a deserialization issue.
+        validate_steps_task = validation_util.validate_log_with_step(validation_with_default_pod_config, start_time=start_time, end_time=end_time)
+
+        # Return a string with the correct bucket path name used in the actual
+        # training
+        validation_gcs_bucket = validation_util.factory_create_validation_config(
+            test_config=test_config,
+            namespace="default",
+            text_filter="Config param checkpoint_dir:",
         )
 
         # We need to get logs from replicator_worker from inside mtc driver.
         # Here we are looking for the string 'Successful: backup for step'. This will tell us that the
         # # checkpoint were backup succesfully. Since all replicator workers need to aggre before the backup
         # We only need logs from one pod.
-        validate_gcs_bucket = log.validate_log_with_gcs(
-            project_id=test_config.cluster.project,
-            location=zone_to_region(test_config.cluster.zone),
-            cluster_name=test_config.cluster.name,
+        validation_with_replicator_pod_config = validation_util.factory_create_validation_config(
+            test_config=test_config,
             text_filter="Successful: backup for step",
             namespace="gke-managed-checkpointing",
-            container_name="replication-worker",
-            pod_pattern="multitier-driver",
-            start_time=start_time,
-            end_time=end_time,
+            gcs_bucket_run_name=validation_gcs_bucket.get_backup_bucket_path() if isinstance(validation_gcs_bucket, validation_util.ValidationPod) else None
         )
+        validate_gcs_bucket_task = validation_util.validate_log_with_gcs(validation_with_replicator_pod_config, start_time=start_time, end_time=end_time)
+
         (
-            init_delete_cpc
-            >> wait_delete_cpc
+            wait_delete_cpc
             >> apply_cpc
             >> start_time
             >> maxtext_chkpt_run_test
             >> ram_disk_cleanup
             >> end_time
-            >> validate_steps
-            >> validate_gcs_bucket
+            >> validate_steps_task
+            >> validate_gcs_bucket_task
         )
