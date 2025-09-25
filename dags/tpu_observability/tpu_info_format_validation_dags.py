ea97095457abe06836a38d927a50d6197322f52e
yuna-tzeng <yuna.tzeng@cienet.com>
2025-09-25 02:03:17 +0000

    refactor(tpu_observability): extract JobSet tasks into `jobset_util`
    This commit moves all JobSet orchestration logic from the main
    tpu_info_format_validation_dag.py file into a dedicated utility file,
    dags/tpu_observability/utils/jobset_util.py.

--- dags/tpu_observability/tpu_info_format_validation_dags.py
+++ dags/tpu_observability/tpu_info_format_validation_dags.py
@@ -24,228 +24,14 @@ from dags.tpu_observability.utils.monitoring import query_time_series
 from dags.tpu_observability.utils import node_pool_util as node_pool
 from dags.tpu_observability.utils.time_util import TimeUtil
 from dags.tpu_observability.utils.tpu_info_util import parse_tpu_info_output
+from dags.tpu_observability.utils import jobset_util as jobset_util
+
 
 
 MACHINE_TYPE_TO_TPU_VERSION = {
     "ct6e-standard-4t": "v6e",
     "ct5p-hightpu-4t": "v5p",
 }
-
-
-def _get_credentials_command(node_pool: node_pool.Info):
-  for attr_name in ["cluster_name", "region", "project_id"]:
-    if not getattr(node_pool, attr_name):
-      raise ValueError(f"{attr_name} must be set in the Info object.")
-
-  return " ".join([
-      "gcloud container clusters",
-      f"get-credentials {node_pool.cluster_name}",
-      f"--region={node_pool.region}",
-      f"--project={node_pool.project_id}",
-  ])
-
-
-def _k8s_apply_jobset_command(
-    kubeconfig: str, yaml_content: str, namespace: str
-):
-  return " ".join([
-      f"kubectl --kubeconfig={kubeconfig} apply",
-      f"-f - -n {namespace} <<EOF\n",
-      f"{yaml_content}\nEOF",
-  ])
-
-
-def _k8s_delete_jobset_command(kubeconfig: str, namespace: str):
-  return " ".join([
-      f"kubectl --kubeconfig={kubeconfig} delete jobsets --all",
-      f"-n {namespace} --timeout=60s --ignore-not-found=true",
-  ])
-
-
-def _k8s_get_pod_command(kubeconfig: str, namespace: str):
-  return " ".join([
-      f"kubectl --kubeconfig={kubeconfig} get pods",
-      f"-n {namespace} -o jsonpath={{.items[*].metadata.name}}",
-  ])
-
-
-@task
-def run_workload(
-    info: node_pool.Info, kubeconfig: str, yaml_config: JobSet, script: str
-) -> TimeUtil:
-  """Applies the specified YAML file to the GKE cluster.
-
-  Args:
-    info: Configuration object with cluster details.
-    kubeconfig: The path to the kubeconfig file.
-    yaml_config: The JobSet object containing YAML configuration.
-    script: The workload script to be executed.
-  """
-  env = os.environ.copy()
-  env["KUBECONFIG"] = kubeconfig
-
-  yaml_content = generator.generate_yaml(workload_script=script)
-
-  result = subprocess.run(
-      " && ".join([
-          _get_credentials_command(info),
-          _k8s_apply_jobset_command(
-              kubeconfig, yaml_content, yaml_config.namespace
-          ),
-      ]),
-      shell=True,
-      check=False,
-      env=env,
-      capture_output=True,
-      text=True,
-  )
-  logging.info(
-      "Command Execute:\n %s",
-      " && ".join([
-          _get_credentials_command(info),
-          _k8s_apply_jobset_command(
-              kubeconfig, yaml_content, yaml_config.namespace
-          ),
-      ]),
-  )
-  if result.returncode != 0:
-    raise AirflowFailException(
-        f"Command failed with exit code {result.returncode}.\n ,STDERR"
-        f" message: {result.stderr}"
-    )
-  logging.info("STDOUT message: %s", result.stdout)
-
-  current_time_utc = datetime.datetime.now(datetime.timezone.utc)
-  return current_time_utc
-
-
-@task
-def end_workload(info: node_pool.Info, kubeconfig: str, yaml_config: JobSet):
-  """Deletes all JobSets from the GKE cluster to clean up resources.
-
-  This task executes a bash script to:
-  1. Authenticate `gcloud` with the specified GKE cluster.
-  2. Delete all JobSets in the `default` namespace using `kubectl`.
-
-  Args:
-    info: Configuration object with cluster details.
-    kubeconfig: The path to the kubeconfig file.
-    yaml_config: The YamlConfig object containing namespace information.
-  """
-  env = os.environ.copy()
-  env["KUBECONFIG"] = kubeconfig
-
-  result = subprocess.run(
-      " && ".join([
-          _get_credentials_command(info),
-          _k8s_delete_jobset_command(kubeconfig, yaml_config.namespace),
-      ]),
-      shell=True,
-      check=False,
-      env=env,
-      capture_output=True,
-      text=True,
-  )
-  if result.returncode != 0:
-    logging.info("Command failed with exit code %s.", result.returncode)
-    logging.info("STDERR message: %s", result.stderr)
-  logging.info("STDOUT message: %s", result.stdout)
-
-
-@task
-def get_active_pods(info: node_pool.Info, kubeconfig: str, yaml_config: JobSet):
-  """Deletes all JobSets from the GKE cluster to clean up resources.
-
-  This task executes a bash script to:
-  1. Authenticate `gcloud` with the specified GKE cluster.
-  2. Delete all JobSets in the `default` namespace using `kubectl`.
-
-  Args:
-    info: Configuration object with cluster details.
-    kubeconfig: The path to the kubeconfig file.
-    yaml_config: The YamlConfig object containing namespace information.
-  """
-  env = os.environ.copy()
-  env["KUBECONFIG"] = kubeconfig
-
-  process = subprocess.run(
-      " && ".join([
-          _get_credentials_command(info),
-          _k8s_get_pod_command(kubeconfig, yaml_config.namespace),
-      ]),
-      shell=True,
-      check=True,
-      env=env,
-      capture_output=True,
-      text=True,
-  )
-
-  if not process or not process.stdout.strip():
-    logging.warning("Received empty pod list from bash task.")
-    raise AirflowFailException("Received empty pod list from bash task.")
-
-  pod_list = process.stdout.strip().split()
-  return pod_list
-
-
-@task.sensor(poke_interval=30, timeout=900, mode="reschedule")
-def wait_for_jobset_started(
-    info: node_pool.Info, pod_name_list: str, job_apply_time: datetime.datetime
-) -> bool:
-  """Waits for the jobset to start by polling Cloud Logging for positive tensorcore utilization metrics.
-
-  This task polls Cloud Logging for a specific log pattern that appears
-  shortly after the TPU job begins execution within the specified container.
-  It times out if no such log is found within a defined period.
-
-  Args:
-    info: An Info dataclass instance containing project and cluster details.
-    pod_name_list: A list of pod names.
-    job_apply_time: The datetime object of the time the job was applied.
-  """
-
-  end_time_datatime = job_apply_time + datetime.timedelta(minutes=10)
-  start_time = TimeUtil.from_datetime(job_apply_time)
-  end_time = TimeUtil.from_datetime(end_time_datatime)
-
-  if not pod_name_list:
-    raise AirflowFailException("pod_name_list is empty, sensor cannot proceed.")
-
-  pod_name = random.choice(pod_name_list)
-  metric_name = "kubernetes.io/container/accelerator/tensorcore_utilization"
-  filter_string = [
-      f'metric.type = "{metric_name}"',
-      f'resource.labels.cluster_name = "{info.cluster_name}"',
-      f'resource.labels.pod_name = "{pod_name}"',
-  ]
-  time_series_data = query_time_series(
-      project_id=info.project_id,
-      filter_str=" AND ".join(filter_string),
-      start_time=start_time,
-      end_time=end_time,
-      view="FULL",
-  )
-
-  # The value of this metric means percentage of tensorcore utilization,
-  # any positive values can represent that the jobset has started.
-  threshold_value: Final[float] = 0.0
-
-  # The minimum number of consecutive initial data points that must all exceed
-  # 'threshold_value' to confirm that the jobset has successfully started and
-  # is active.
-  threshold_records_count: Final[int] = 3
-
-  if (
-      not time_series_data
-      or len(time_series_data[0].points) < threshold_records_count
-  ):
-    return False
-  last_n_data_points = [
-      round(point.value.double_value, 2)
-      for point in time_series_data[0].points[0:threshold_records_count]
-  ]
-
-  return all(p > threshold_value for p in last_n_data_points)
 
 
 @task
@@ -478,36 +264,26 @@ with models.DAG(
       tpu_accelerator_type="tpu-v6e-slice",
       tpu_topology="4x4",
       container_name="jax-tpu-worker",
-      image="us-docker.pkg.dev/tpu-prod-env-one-vm/yuna-docker-repo/tpu-info:v0.4.0",
+      image="us-docker.pkg.dev/tpu-prod-env-one-vm/yuna-docker-repo/tpu-info:v0.5.1",
       tpu_cores_per_pod=4,
   )
 
   workload_script = Workload.JAX_TPU_BENCHMARK
 
-  # Clean up any pre-existing workloads to ensure a clean environment for the
-  # test.
-  start_cleanup = end_workload.override(
-      task_id="start_cleanup_workload", trigger_rule=TriggerRule.ALL_DONE
-  )(
-      info=cluster_info,
-      kubeconfig=kubeconfig_path,
-      yaml_config=generator,
-  )
-
-  apply_time = run_workload(
+  apply_time = jobset_util.run_workload(
       info=cluster_info,
       kubeconfig=kubeconfig_path,
       yaml_config=generator,
       script=workload_script,
   )
 
-  active_pods = get_active_pods.override(task_id="get_active_pod")(
+  active_pods = jobset_util.get_active_pods.override(task_id="get_active_pod")(
       info=cluster_info,
       kubeconfig=kubeconfig_path,
       yaml_config=generator,
   )
 
-  wait_for_job_start = wait_for_jobset_started.override(
+  wait_for_job_start = jobset_util.wait_for_jobset_started.override(
       task_id="wait_for_job_start"
   )(cluster_info, pod_name_list=active_pods, job_apply_time=apply_time)
 
@@ -554,7 +330,7 @@ with models.DAG(
         .expand(tpu_info_dict=tpu_info_dict)
     )
 
-  clean_up = end_workload.override(
+  clean_up = jobset_util.end_workload.override(
       task_id="clean_up_workload", trigger_rule=TriggerRule.ALL_DONE
   )(
       info=cluster_info,
@@ -575,8 +351,7 @@ with models.DAG(
   )
 
   (
-      start_cleanup
-      >> apply_time
+      apply_time
       >> active_pods
       >> wait_for_job_start
       >> tpu_info_outputs
---
+++ dags/tpu_observability/utils/jobset_util.py
@@ -0,0 +1,230 @@
+import datetime
+import logging
+import os
+import random
+import subprocess
+from typing import Dict, Final, List, Tuple
+
+from airflow.decorators import task
+from airflow.exceptions import AirflowFailException
+from dags.tpu_observability.utils.jobset_generator import JobSet
+from dags.tpu_observability.utils.monitoring import query_time_series
+from dags.tpu_observability.utils import node_pool_util as node_pool
+from dags.tpu_observability.utils.time_util import TimeUtil
+
+
+def _get_credentials_command(node_pool: node_pool.Info):
+  for attr_name in ["cluster_name", "region", "project_id"]:
+    if not getattr(node_pool, attr_name):
+      raise ValueError(f"{attr_name} must be set in the Info object.")
+
+  return " ".join([
+      "gcloud container clusters",
+      f"get-credentials {node_pool.cluster_name}",
+      f"--region={node_pool.region}",
+      f"--project={node_pool.project_id}",
+  ])
+
+
+def _k8s_apply_jobset_command(
+    kubeconfig: str, yaml_content: str, namespace: str
+):
+  return " ".join([
+      f"kubectl --kubeconfig={kubeconfig} apply",
+      f"-f - -n {namespace} <<EOF\n",
+      f"{yaml_content}\nEOF",
+  ])
+
+
+def _k8s_delete_jobset_command(kubeconfig: str, namespace: str):
+  return " ".join([
+      f"kubectl --kubeconfig={kubeconfig} delete jobsets --all",
+      f"-n {namespace} --timeout=60s --ignore-not-found=true",
+  ])
+
+
+def _k8s_get_pod_name_command(kubeconfig: str, namespace: str):
+  return " ".join([
+      f"kubectl --kubeconfig={kubeconfig} get pods",
+      f"-n {namespace} -o jsonpath={{.items[*].metadata.name}}",
+  ])
+
+
+@task
+def run_workload(
+    info: node_pool.Info, kubeconfig: str, yaml_config: JobSet, script: str
+) -> TimeUtil:
+  """Applies the specified YAML file to the GKE cluster.
+
+  Args:
+    info: Configuration object with cluster details.
+    kubeconfig: The path to the kubeconfig file.
+    yaml_config: The JobSet object containing YAML configuration.
+    script: The workload script to be executed.
+  """
+  env = os.environ.copy()
+  env["KUBECONFIG"] = kubeconfig
+
+  yaml_content = yaml_config.generate_yaml(workload_script=script)
+
+  result = subprocess.run(
+      " && ".join([
+          _get_credentials_command(info),
+          _k8s_apply_jobset_command(
+              kubeconfig, yaml_content, yaml_config.namespace
+          ),
+      ]),
+      shell=True,
+      check=False,
+      env=env,
+      capture_output=True,
+      text=True,
+  )
+  logging.info(
+      "Command Execute:\n %s",
+      " && ".join([
+          _get_credentials_command(info),
+          _k8s_apply_jobset_command(
+              kubeconfig, yaml_content, yaml_config.namespace
+          ),
+      ]),
+  )
+  if result.returncode != 0:
+    raise AirflowFailException(
+        f"Command failed with exit code {result.returncode}.\n ,STDERR"
+        f" message: {result.stderr}"
+    )
+  logging.info("STDOUT message: %s", result.stdout)
+
+  current_time_utc = datetime.datetime.now(datetime.timezone.utc)
+  return current_time_utc
+
+
+@task
+def end_workload(info: node_pool.Info, kubeconfig: str, yaml_config: JobSet):
+  """Deletes all JobSets from the GKE cluster to clean up resources.
+
+  This task executes a bash script to:
+  1. Authenticate `gcloud` with the specified GKE cluster.
+  2. Delete all JobSets in the `default` namespace using `kubectl`.
+
+  Args:
+    info: Configuration object with cluster details.
+    kubeconfig: The path to the kubeconfig file.
+    yaml_config: The YamlConfig object containing namespace information.
+  """
+  env = os.environ.copy()
+  env["KUBECONFIG"] = kubeconfig
+
+  result = subprocess.run(
+      " && ".join([
+          _get_credentials_command(info),
+          _k8s_delete_jobset_command(kubeconfig, yaml_config.namespace),
+      ]),
+      shell=True,
+      check=False,
+      env=env,
+      capture_output=True,
+      text=True,
+  )
+  if result.returncode != 0:
+    logging.info("Command failed with exit code %s.", result.returncode)
+    logging.info("STDERR message: %s", result.stderr)
+  logging.info("STDOUT message: %s", result.stdout)
+
+
+@task
+def get_active_pods(info: node_pool.Info, kubeconfig: str, yaml_config: JobSet):
+  """Deletes all JobSets from the GKE cluster to clean up resources.
+
+  This task executes a bash script to:
+  1. Authenticate `gcloud` with the specified GKE cluster.
+  2. Delete all JobSets in the `default` namespace using `kubectl`.
+
+  Args:
+    info: Configuration object with cluster details.
+    kubeconfig: The path to the kubeconfig file.
+    yaml_config: The YamlConfig object containing namespace information.
+  """
+  env = os.environ.copy()
+  env["KUBECONFIG"] = kubeconfig
+
+  process = subprocess.run(
+      " && ".join([
+          _get_credentials_command(info),
+          _k8s_get_pod_name_command(kubeconfig, yaml_config.namespace),
+      ]),
+      shell=True,
+      check=True,
+      env=env,
+      capture_output=True,
+      text=True,
+  )
+
+  if not process or not process.stdout.strip():
+    logging.warning("Received empty pod list from bash task.")
+    raise AirflowFailException("Received empty pod list from bash task.")
+
+  pod_list = process.stdout.strip().split()
+  return pod_list
+
+
+@task.sensor(poke_interval=30, timeout=900, mode="reschedule")
+def wait_for_jobset_started(
+    info: node_pool.Info, pod_name_list: str, job_apply_time: datetime.datetime
+) -> bool:
+  """Waits for the jobset to start by polling Cloud Logging for positive tensorcore utilization metrics.
+
+  This task polls Cloud Logging for a specific log pattern that appears
+  shortly after the TPU job begins execution within the specified container.
+  It times out if no such log is found within a defined period.
+
+  Args:
+    info: An Info dataclass instance containing project and cluster details.
+    pod_name_list: A list of pod names.
+    job_apply_time: The datetime object of the time the job was applied.
+  """
+
+  end_time_datatime = job_apply_time + datetime.timedelta(minutes=10)
+  start_time = TimeUtil.from_datetime(job_apply_time)
+  end_time = TimeUtil.from_datetime(end_time_datatime)
+
+  if not pod_name_list:
+    raise AirflowFailException("pod_name_list is empty, sensor cannot proceed.")
+
+  pod_name = random.choice(pod_name_list)
+  metric_name = "kubernetes.io/container/accelerator/tensorcore_utilization"
+  filter_string = [
+      f'metric.type = "{metric_name}"',
+      f'resource.labels.cluster_name = "{info.cluster_name}"',
+      f'resource.labels.pod_name = "{pod_name}"',
+  ]
+  time_series_data = query_time_series(
+      project_id=info.project_id,
+      filter_str=" AND ".join(filter_string),
+      start_time=start_time,
+      end_time=end_time,
+      view="FULL",
+  )
+
+  # The value of this metric means percentage of tensorcore utilization,
+  # any positive values can represent that the jobset has started.
+  threshold_value: Final[float] = 0.0
+
+  # The minimum number of consecutive initial data points that must all exceed
+  # 'threshold_value' to confirm that the jobset has successfully started and
+  # is active.
+  threshold_records_count: Final[int] = 3
+
+  if (
+      not time_series_data
+      or len(time_series_data[0].points) < threshold_records_count
+  ):
+    return False
+  last_n_data_points = [
+      round(point.value.double_value, 2)
+      for point in time_series_data[0].points[0:threshold_records_count]
+  ]
+
+  return all(p > threshold_value for p in last_n_data_points)
+
