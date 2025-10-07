8f3def2323a9572cabe2763d72fa40603fac9295
Ryan Liu <ryan.liu@cienet.com>
2025-10-07 18:33:24 +0800

    Update DAG change_node_pool_label.py

--- dags/tpu_observability/change_node_pool_label.py
+++ dags/tpu_observability/change_node_pool_label.py
@@ -1,13 +1,59 @@
-"""A DAG to validate the status of a GKE node pool after changing its node label."""
+"""A DAG to validate the status of a GKE node pool after changing its label."""
 
 import datetime
+import logging
 
 from airflow import models
+from airflow.exceptions import AirflowFailException
+from airflow.operators.python import BranchPythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.task_group import TaskGroup
 from airflow.utils.trigger_rule import TriggerRule
 
 from dags.common.vm_resource import Project, Region, Zone
 from dags.map_reproducibility.utils import constants
+from dags.tpu_observability.configs.common import MachineConfigMap
 from dags.tpu_observability.utils import node_pool_util as node_pool
+
+
+_THRESHOLD_SECONDS = 150.0
+
+
+def check_duration_and_determine_branch(**kwargs) -> str:
+  """Determines which task to do next based on the given duration and threshold
+
+  Reads the duration from XCom and returns the next Task ID
+  based on the threshold.
+
+  Returns:
+      The Task ID ('wait_for_ttr' or 'skip_ttr_check') to proceed to.
+  Raises:
+      RuntimeError: If the operation duration could not be retrieved from XCom.
+  """
+  ti = kwargs["ti"]
+  duration_seconds = ti.xcom_pull(
+    task_ids=f"v{config.tpu_version.value}.get_node_pool_update_duration",
+    key="return_value"
+  )
+
+  if duration_seconds is None:
+    error_msg = (
+        "No update duration found."
+    )
+    raise AirflowFailException(error_msg)
+
+  if duration_seconds >= _THRESHOLD_SECONDS:
+    logging.info(
+        f"Duration ({duration_seconds:.2f}s) >= {_THRESHOLD_SECONDS}s. "
+        f"Proceeding to TTR check."
+    )
+    return f"v{config.tpu_version.value}.wait_for_ttr"
+  else:
+    logging.info(
+        f"Duration ({duration_seconds:.2f}s) < {_THRESHOLD_SECONDS}s. "
+        f"Skipping TTR check."
+    )
+    return f"v{config.tpu_version.value}.skip_ttr_check"
 
 
 with models.DAG(
@@ -25,7 +71,8 @@ with models.DAG(
 
       ### Description
       This DAG automates the process of creating a GKE
-      node pool, changing its node pool label, and verifying whether the node pool status is reported correctly.
+      node pool, changing its node pool label, and verifying whether the node pool
+      status is reported correctly.
 
       ### Prerequisites
       This test requires an existing cluster.
@@ -37,72 +84,91 @@ with models.DAG(
       The node pool will be cleaned up after the tests.
     """,
 ) as dag:
-  node_pool_info = node_pool.Info(
-      project_id=models.Variable.get(
-          "PROJECT_ID", default_var=Project.TPU_PROD_ENV_ONE_VM.value
-      ),
-      cluster_name=models.Variable.get(
-          "CLUSTER_NAME", default_var="ryan-cluster-asia"
-      ),
-      node_pool_name=models.Variable.get(
-          "NODE_POOL_NAME", default_var="ryan-v6e-autotest"
-      ),
-      location=models.Variable.get(
-          "LOCATION", default_var=Region.ASIA_NORTHEAST1.value
-      ),
-      node_locations=models.Variable.get(
-          "NODE_LOCATIONS", default_var=Zone.ASIA_NORTHEAST1_B.value
-      ),
-      num_nodes=models.Variable.get("NUM_NODES", default_var=4),
-      machine_type=models.Variable.get(
-          "MACHINE_TYPE", default_var="ct6e-standard-4t"
-      ),
-      tpu_topology=models.Variable.get("TPU_TOPOLOGY", default_var="4x4"),
-  )
+  for machine in MachineConfigMap:
+    config = machine.value
+    node_pool_info = node_pool.Info(
+        project_id=models.Variable.get(
+            "PROJECT_ID", default_var="cienet-cmcs"
+        ),
+        cluster_name=models.Variable.get(
+            "CLUSTER_NAME", default_var="tpu-observability-automation"
+        ),
+        node_pool_name=models.Variable.get(
+            "NODE_POOL_NAME", default_var="change-node-pool-label-v6e-autotest"
+        ),
+        location=models.Variable.get(
+            "LOCATION", default_var=Region.US_CENTRAL1.value
+        ),
+        node_locations=models.Variable.get(
+            "NODE_LOCATIONS", default_var=Zone.US_CENTRAL1_B.value
+        ),
+        num_nodes=models.Variable.get("NUM_NODES", default_var=4),
+        machine_type=config.machine_version.value,
+        tpu_topology=config.tpu_topology,
+    )
 
-  task_id = "create_node_pool"
-  create_node_pool = node_pool.create.override(task_id=task_id)(
-      node_pool=node_pool_info
-  )
+    with TaskGroup(group_id=f"v{config.tpu_version.value}"):
+      task_id = "create_node_pool"
+      create_node_pool = node_pool.create.override(task_id=task_id)(
+          node_pool=node_pool_info,
+          reservation="cloudtpu-20251107233000-1246578561"
+      )
 
-  task_id = "wait_for_provisioning"
-  wait_for_provisioning = node_pool.wait_for_status.override(task_id=task_id)(
-      node_pool=node_pool_info, status=node_pool.Status.PROVISIONING
-  )
+      task_id = "wait_for_provisioning"
+      wait_for_provisioning = node_pool.wait_for_status.override(task_id=task_id)(
+          node_pool=node_pool_info, status=node_pool.Status.PROVISIONING
+      )
 
-  task_id = "wait_for_running"
-  wait_for_running = node_pool.wait_for_status.override(task_id=task_id)(
-      node_pool=node_pool_info, status=node_pool.Status.RUNNING
-  )
+      task_id = "wait_for_running"
+      wait_for_running = node_pool.wait_for_status.override(
+          task_id=task_id
+      )(node_pool=node_pool_info, status=node_pool.Status.RUNNING)
 
-  task_id = "change_node_label"
-  change_node_label = node_pool.change_node_label.override(task_id=task_id)(
-      node_pool=node_pool_info
-  )
+      task_id = "change_node_pool_label"
+      change_node_pool_label = node_pool.change_node_pool_label.override(
+          task_id=task_id
+      )(node_pool=node_pool_info)
 
-  task_id = "wait_for_reconciling"
-  wait_for_reconciling = node_pool.wait_for_status.override(task_id=task_id)(
-      node_pool=node_pool_info, status=node_pool.Status.RECONCILING
-  )
+      task_id = "wait_for_recovered"
+      wait_for_recovered = node_pool.wait_for_status.override(task_id=task_id)(
+          node_pool=node_pool_info, status=node_pool.Status.RUNNING
+      )
 
-  task_id = "wait_for_recovered"
-  wait_for_recovered = node_pool.wait_for_status.override(task_id=task_id)(
-      node_pool=node_pool_info, status=node_pool.Status.RUNNING
-  )
+      task_id = "get_node_pool_update_duration"
+      get_node_pool_update_duration = node_pool.get_node_pool_update_duration.override(
+          task_id=task_id
+      )(node_pool=node_pool_info)
 
-  task_id = "cleanup_node_pool"
-  cleanup_node_pool = node_pool.delete.override(
-      task_id=task_id, trigger_rule=TriggerRule.ALL_DONE
-  )(node_pool=node_pool_info).as_teardown(
-      setups=create_node_pool,
-  )
+      task_id = "determine_next_branch"
+      determine_next_branch = BranchPythonOperator(
+          task_id=task_id,
+          python_callable=check_duration_and_determine_branch,
+          dag=dag,
+      )
 
-  normal_flow = (
-      create_node_pool
-      >> wait_for_provisioning
-      >> wait_for_running
-      >> change_node_label
-      >> wait_for_reconciling
-      >> wait_for_recovered
+      # Task: if node pool update duration >= 150 seconds, do the TTR check.
+      task_id = "wait_for_ttr"
+      wait_for_ttr = node_pool.wait_for_ttr.override(task_id=task_id)(
+          node_pool=node_pool_info
+      )
+
+      # Task: if node pool update duration < 150 seconds, skip the TTR check.
+      task_id = "skip_ttr_check"
+      skip_ttr_check = EmptyOperator(task_id=task_id)
+
+      task_id = "cleanup_node_pool"
+      cleanup_node_pool = node_pool.delete.override(
+          task_id=task_id, trigger_rule=TriggerRule.ALL_DONE
+      )(node_pool=node_pool_info).as_teardown(
+          setups=create_node_pool,
+      )
+
+      create_node_pool \
+      >> wait_for_provisioning \
+      >> wait_for_running \
+      >> change_node_pool_label \
+      >> wait_for_recovered \
+      >> get_node_pool_update_duration \
+      >> determine_next_branch \
+      >> [wait_for_ttr, skip_ttr_check] \
       >> cleanup_node_pool
-  )
