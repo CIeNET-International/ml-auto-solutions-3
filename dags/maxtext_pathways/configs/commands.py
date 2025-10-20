de4899b97d745ee6425c16f5709ea54dc2f7fa00
juliekuo <juliekuo@google.com>
2025-10-20 09:20:38 +0800

    refactor: Restructure and enhance configuration and operator usage
    This commit refactors several core components and introduces new configuration options for better flexibility and maintainability.
    
    Major changes include:
    * Replace BashOperator with SubprocessHook for better pod deletion handling.
    * Refactor task parameters variables for clearer separation.
    * Change `recipe_name` from string to enum for later expansion.
    * Introduce custom Model Name option.
    * Add dynamic timeout calculation based on `step * min`, along with a new enable button.

--- dags/maxtext_pathways/configs/commands.py
+++ dags/maxtext_pathways/configs/commands.py
@@ -12,18 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-COMMAND_INSTALL_MAKE = [
+UPDATE_APT = [
     "sudo apt-get update",
+]
+
+INSTALL_MAKE = [
     "sudo apt-get install -y make",
 ]
 
-COMMAND_INSTALL_KUBECTL = [
+INSTALL_KUBECTL = [
     "grep -rhE ^deb /etc/apt/sources.list* | grep 'cloud-sdk'",
     "sudo apt-get install -y kubectl",
     "kubectl version --client",
 ]
 
-COMMAND_INSTALL_DOCKER = [
+INSTALL_DOCKER = [
     "sudo apt-get install ca-certificates curl",
     "sudo install -m 0755 -d /etc/apt/keyrings",
     "sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc",
@@ -32,38 +35,31 @@ COMMAND_INSTALL_DOCKER = [
     "sudo apt-get update",
     "sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
     "sudo /usr/bin/dockerd & sleep 0",
-    "while ! sudo docker info > /dev/null 2>&1; do echo 'Waiting for Docker to start...'; sleep 2; done",
+    "while ! sudo docker info > /dev/null 2>&1; do echo 'Waiting for Docker to start...'; sleep 5; done",  # check whether Docker is started successfully
 ]
 
 
-COMMAND_INSTALL_GCLOUD = [
+INSTALL_GCLOUD = [
     "curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz",
     "tar -xf google-cloud-cli-linux-x86_64.tar.gz",
     "./google-cloud-sdk/install.sh",
     "./google-cloud-sdk/bin/gcloud init",
 ]
 
-COMMAND_SWITCH_SERVICE_ACCOUNT = [
-    "mkdir -p /root/keys",
-    'gcloud secrets versions access "latest" --secret="one-click-key" > /root/keys/sa_key.json',
-    "gcloud config set account {service_account}",
-    "gcloud auth activate-service-account {service_account} --key-file=/root/keys/sa_key.json",
-]
-
-COMMAND_INSTALL_KUBECTL_KJOB = [
+INSTALL_KUBECTL_KJOB = [
     "curl -Lo ./kubectl-kjob https://github.com/kubernetes-sigs/kjob/releases/download/v0.1.0/kubectl-kjob-linux-amd64",
     "chmod +x ./kubectl-kjob",
     "sudo mv ./kubectl-kjob /usr/local/bin/kubectl-kjob",
 ]
 
-COMMAND_INSTALL_KUBECTL_KUEUE = [
+INSTALL_KUBECTL_KUEUE = [
     "curl -Lo ./kubectl-kueue https://github.com/kubernetes-sigs/kueue/releases/download/v0.13.3/kubectl-kueue-linux-amd64",
     "chmod +x ./kubectl-kueue",
     "sudo mv ./kubectl-kueue /usr/local/bin/kubectl-kueue",
 ]
 
-COMMAND_INSTALL_XPK = [
-    "git clone https://github.com/google/xpk.git /root/xpk",
+INSTALL_XPK = [
+    "git clone --branch v0.12.0 https://github.com/AI-Hypercomputer/xpk /root/xpk",
     "cd /root/xpk",
     "python3 -m venv --system-site-packages .venv",
     "source .venv/bin/activate",
@@ -71,41 +67,29 @@ COMMAND_INSTALL_XPK = [
     "export PATH=$PATH:$HOME/xpk/bin",
 ]
 
-COMMAND_BACK_MAXTEXT = [
+SWITCH_SERVICE_ACCOUNT = [
+    "mkdir -p /root/keys",
+    'gcloud secrets versions access "latest" --secret="one-click-key" > /root/keys/sa_key.json',
+    "gcloud config set account {service_account}",
+    "gcloud auth activate-service-account {service_account} --key-file=/root/keys/sa_key.json",
+]
+
+BACK_MAXTEXT = [
     "cd /deps",  # back to maxtext folder
     "export USER=root",  # give it a USER to `pip install --upgrade pip`
 ]
 
-COMMAND_RUN_RECIPE = ["python3 -m benchmarks.recipes.pw_mcjax_benchmark_recipe"]
+RUN_RECIPE = ["python3 -m benchmarks.recipes.{recipe_name}"]
 
-COMMAND_DELETE_POD = [
-    "set -xue",
-    "export KUBECONFIG=/tmp/kubeconfig", # Change KUBECONFIG from /home/airflow to /tmp to avoid permission issue.
-    "gcloud container clusters get-credentials {cluster_name} --region={region} --project={project}",
-    "kubectl delete pod -l airflow-runtime={airflow_runtime} --namespace=default --force --grace-period=0",
-]
-
-COMMAND_ENV = (
-    COMMAND_INSTALL_MAKE
-    + COMMAND_INSTALL_KUBECTL
-    + COMMAND_INSTALL_DOCKER
-    + COMMAND_INSTALL_GCLOUD
-    + COMMAND_SWITCH_SERVICE_ACCOUNT
-    + COMMAND_INSTALL_KUBECTL_KJOB
-    + COMMAND_INSTALL_KUBECTL_KUEUE
-    + COMMAND_INSTALL_XPK
-    + COMMAND_BACK_MAXTEXT
+ENV_COMMAND = (
+    UPDATE_APT
+    + INSTALL_MAKE
+    + INSTALL_KUBECTL
+    + INSTALL_DOCKER
+    + INSTALL_GCLOUD
+    + SWITCH_SERVICE_ACCOUNT
+    + INSTALL_KUBECTL_KJOB
+    + INSTALL_KUBECTL_KUEUE
+    + INSTALL_XPK
+    + BACK_MAXTEXT
 )
-
-COMMAND_RECIPE = COMMAND_RUN_RECIPE
-
-COMMAND_ENV = " && ".join(COMMAND_ENV)
-COMMAND_RECIPE = " && ".join(COMMAND_RECIPE)
-COMMAND_DELETE_POD_STR = " && ".join(COMMAND_DELETE_POD)
-
-
-if __name__ == "__main__":
-    print(f"{COMMAND_ENV = }\n")
-    print(f"{COMMAND_RECIPE = }\n")
-    TOTAL_COMMAND = " && ".join([COMMAND_ENV, COMMAND_RECIPE])
-    print(f"{TOTAL_COMMAND = }\n")
--- dags/maxtext_pathways/pw_mcjax_benchmark_recipe.py
+++ dags/maxtext_pathways/pw_mcjax_benchmark_recipe.py
@@ -14,18 +14,19 @@
 
 import datetime
 from airflow.models.dag import DAG
-from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
 from kubernetes.client import models as k8s
 
 from dags.common.vm_resource import DockerImage
-from dags.maxtext_pathways.configs.parameters import PARAMETERS
-from dags.maxtext_pathways.configs.commands import COMMAND_DELETE_POD_STR
-from dags.maxtext_pathways.utils.tasks import get_parameters, set_sensor_timeout
+from dags.maxtext_pathways.configs import parameters as ui_params
+from dags.maxtext_pathways.configs import recipe_config as recipe_cfg
+from dags.maxtext_pathways.utils import tasks
 from xlml.utils.xpk import wait_for_workload_completion, clean_up_workload
 
 
-RECIPE = "pw_mcjax_benchmark_recipe"
+RECIPE_NAME = recipe_cfg.RecipeConfigs.PW_MCJAX_BENCHMARK_RECIPE.value
+RECIPE = RECIPE_NAME.lower()
+
 with DAG(
     dag_id=RECIPE,
     start_date=datetime.datetime(2025, 1, 1),
@@ -42,7 +43,7 @@ with DAG(
         "nightly",
     ],
     description=f"A DAG to run a MaxText {RECIPE} on GKE.",
-    params=PARAMETERS,
+    params=ui_params.PARAMETERS,
     doc_md=f"""
   # A DAG to run a MaxText {RECIPE} on GKE.
 
@@ -54,7 +55,8 @@ with DAG(
   - This test requires an existing cluster.
   - This test requires that a dataset with the same name as the UI parameter "[BigQuery Database Dataset]".
   - Create a service account named `one-click` with the following roles: `Artifact Registry Reader`, `Kubernetes Engine Admin`, `Monitoring Viewer`.
-    - Generate a new service account key and download the JSON file to retrieve its contents. Next, create a secret manager named `one-click-key` and store the key contents there for use when switching service accounts.
+    - Generate a new service account key and download the JSON file to retrieve its contents. 
+      Next, create a secret manager named `one-click-key` and store the key contents there for use when switching service accounts.
     - Make sure the default service account has the `Secret Manager Secret Accessor` role.  
       ex: [PROJECT_NUMBER]-compute@developer.gserviceaccount.com
   - If you're using a service account to pull an image from a different project, you need to grant the service account the `Artifact Registry Reader` role in that project.
@@ -67,57 +69,64 @@ with DAG(
   If you want to add other TPU type models, you need to manually modify `/ml-auto-solutions/dags/maxtext_pathways/configs/model_configs.py`.
   """,
 ) as dag:
-  # Define task dependencies by instantiating and linking tasks.
-  params = get_parameters()
-
   recipe_runtime = (
       RECIPE.replace("_", "-") + '-{{ execution_date.strftime("%H%M%S") }}'
   )
 
+  # Define task dependencies by instantiating and linking tasks.
+  dag_params = tasks.get_dag_parameters()
+  derived_params = tasks.generate_derived_parameters(dag_params)
+  commands = tasks.generate_commands(dag_params, derived_params, RECIPE_NAME)
+
   start_recipe = GKEStartPodOperator(
       task_id="start_recipe",
       name=RECIPE.replace("_", "-"),
-      project_id=params["project"],
-      cluster_name=params["cluster_name"],
-      location=params["region"],
+      project_id=dag_params["project"],
+      cluster_name=dag_params["cluster_name"],
+      location=derived_params["region"],
       namespace="default",
       hostnetwork=True,
       image=DockerImage.MAXTEXT_TPU_JAX_NIGHTLY.value,
-      # on_finish_action=OnFinishAction.DELETE_POD.value,  # TODO: Unable to delete the pod, may need to update the Airflow version.
+      # TODO(b/452777428): Unable to delete the pod, may need to update the Airflow version.
+      # on_finish_action=OnFinishAction.DELETE_POD.value,
       get_logs=True,
-      cmds=["/bin/bash", "-cxue", params["commands"]],
+      cmds=["/bin/bash", "-cxue", commands],
       container_security_context=k8s.V1SecurityContext(privileged=True),
       labels={"airflow-runtime": recipe_runtime},
   )
 
-  clean_up_pod = BashOperator(
-      task_id="clean_up_pod",
-      bash_command=(
-          COMMAND_DELETE_POD_STR.format(
-              cluster_name=params["cluster_name"],
-              region=params["region"],
-              project=params["project"],
-              airflow_runtime=recipe_runtime,
-          )
-      ),
-  ).as_teardown(setups=params)
+  clean_up_pod = tasks.clean_up_pod(
+      cluster_name=dag_params["cluster_name"],
+      region=derived_params["region"],
+      project=dag_params["project"],
+      airflow_runtime=recipe_runtime,
+  ).as_teardown(setups=[commands])
 
   check_recipe_log = wait_for_workload_completion.override(
-      task_id="check_recipe_log", on_execute_callback=[set_sensor_timeout]
+      task_id="check_recipe_log",
+      poke_interval=30,
+      on_execute_callback=[tasks.set_sensor_timeout],
   )(
-      workload_id=params["recipe_workload_id"],
-      project_id=params["project"],
-      region=params["region"],
-      cluster_name=params["cluster_name"],
+      workload_id=derived_params["recipe_workload_id"],
+      project_id=dag_params["project"],
+      region=derived_params["region"],
+      cluster_name=dag_params["cluster_name"],
   )
 
   clean_up_recipe = clean_up_workload.override(task_id="clean_up_recipe")(
-      workload_id=params["recipe_workload_id"],
-      project_id=params["project"],
-      zone=params["zone"],
-      cluster_name=params["cluster_name"],
-  ).as_teardown(setups=start_recipe)
+      workload_id=derived_params["recipe_workload_id"],
+      project_id=dag_params["project"],
+      zone=dag_params["zone"],
+      cluster_name=dag_params["cluster_name"],
+  ).as_teardown(setups=[start_recipe])
 
   # Set the execution order.
-  params >> start_recipe >> check_recipe_log >> clean_up_recipe
+  (
+      dag_params
+      >> derived_params
+      >> commands
+      >> start_recipe
+      >> check_recipe_log
+      >> clean_up_recipe
+  )
   start_recipe >> clean_up_pod
