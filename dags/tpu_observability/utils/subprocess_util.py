cf84ff2ca0ca10fd37ad0909570670de654864b4
yuna-tzeng <yuna.tzeng@cienet.com>
2025-11-21 02:28:26 +0000

    fix: Add comment in subprocess_util file

--- dags/tpu_observability/utils/subprocess_util.py
+++ dags/tpu_observability/utils/subprocess_util.py
@@ -9,7 +9,7 @@ from airflow.exceptions import AirflowFailException
 def run_exec(
     cmd: str,
     env: dict[str, str] | None = None,
-    log_command=False,
+    log_command: bool = False,
     log_output: bool = True,
 ) -> str:
   """Executes a shell command and logs its output."""
@@ -17,12 +17,18 @@ def run_exec(
     logging.info("[subprocess] executing command:\n %s\n", cmd)
 
   res = subprocess.run(
-      cmd,
+      cmd,  # The command string to execute (e.g., 'ls -l | grep file')
+      # Optional: Environment variables to use for the command
+      # (overrides parent environment)
       env=env,
-      shell=True,
+      shell=True,  # REQUIRED for shell features (e.g., pipes, redirects).
+      # Optional: Do NOT raise CalledProcessError if the command returns a
+      # non-zero exit code. We handle errors manually.
       check=False,
+      # Optional: Capture stdout and stderr in the result object.
       capture_output=True,
-      text=True,
+      text=True,  # Optional: Decode stdout/stderr output as text strings
+      # (using the default system encoding).
   )
 
   if res.returncode != 0:
