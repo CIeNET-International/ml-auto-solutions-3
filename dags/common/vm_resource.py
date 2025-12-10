b34024953a459884c7788ee00b1d614edb2d525a
Alfred Yu <alfred.yu@cienet.com>
2025-12-10 10:47:19 +0800

    PR #75 squash and resolve some conflicts

--- dags/common/vm_resource.py
+++ dags/common/vm_resource.py
@@ -332,6 +332,10 @@ class DockerImage(enum.Enum):
       "us-central1-docker.pkg.dev/tpu-pytorch-releases/docker/"
       f"xla:nightly_3.10_tpuvm_{datetime.datetime.today().strftime('%Y%m%d')}"
   )
+  AXLEARN_CUSTOM = (
+      "gcr.io/cloud-tpu-multipod-dev/axlearn-custom:"
+      f"{datetime.datetime.today().strftime('%Y-%m-%d')}"
+  )
   AXLEARN_TPU_JAX_STABLE_STACK = (
       "us-docker.pkg.dev/tpu-prod-env-multipod/bite/tpu/axlearn:"
       f"{datetime.datetime.today().strftime('%Y-%m-%d')}"
