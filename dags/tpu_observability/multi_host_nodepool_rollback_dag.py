42458fbb995b85c2e44262f8da51486704218b49
EmmaLien <emma.lien@cienet.com>
2025-12-23 11:08:55 +0800

    fix: change DAG trigger time to nightime (2AM-4AM) TST for better observability

--- dags/tpu_observability/multi_host_nodepool_rollback_dag.py
+++ dags/tpu_observability/multi_host_nodepool_rollback_dag.py
@@ -25,7 +25,7 @@ from airflow.utils.trigger_rule import TriggerRule
 
 from dags import composer_env
 from dags.common import test_owner
-from dags.map_reproducibility.utils import constants
+from dags.tpu_observability.utils.constants import Schedule
 from dags.tpu_observability.configs.common import MachineConfigMap, GCS_CONFIG_PATH
 from dags.tpu_observability.utils import node_pool_util as node_pool
 
@@ -35,7 +35,7 @@ from dags.tpu_observability.utils import node_pool_util as node_pool
 with models.DAG(  # pylint: disable=unexpected-keyword-arg
     dag_id="multi-host-availability-rollback",
     start_date=datetime.datetime(2025, 8, 10),
-    schedule=constants.Schedule.DAILY_PST_6_30PM,
+    schedule=Schedule.DAILY_TST_2_30AM if composer_env.is_prod_env() else None,
     catchup=False,
     tags=[
         "cloud-ml-auto-solutions",
