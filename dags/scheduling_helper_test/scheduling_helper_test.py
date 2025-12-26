9576d08201e13c2f6996246c6eca10f90f477b85
Kim Pai <kim.pai@cienet.com>
2025-12-26 16:27:33 +0800

    test : add scheduling helper test files

---
+++ dags/scheduling_helper_test/scheduling_helper_test.py
@@ -0,0 +1,13 @@
+from dags.scheduling_helper_test.scheduling_helper import SchedulingHelper
+from dags.common.vm_resource import XpkClusters
+
+
+if __name__ == "__main__":
+  for i in range(1, 13):
+    try:
+      test_case = SchedulingHelper.ArrangeScheduleTime(
+          XpkClusters.TPU_V5P_128_CLUSTER, f"test{i}"
+      )
+      print(f"Test case {i} schedule: {test_case}")
+    except Exception as e:
+      print(f"Test case {i} raised an error: {e}")
