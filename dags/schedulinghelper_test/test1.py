08e0f794acfda723a77ad09d4fadbc38b0cfcb4f
Kim Pai <kim.pai@cienet.com>
2025-12-26 12:40:14 +0800

    add : test dag

---
+++ dags/schedulinghelper_test/test1.py
@@ -0,0 +1,34 @@
+
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime, timedelta
+import time
+from dags.common.vm_resource import XpkClusters
+from dags.schedulinghelper_test.scheduling_helper import SchedulingHelper
+
+
+DAG_ID = "test1"
+SCHEDULE = SchedulingHelper.ArrangeScheduleTime(
+    XpkClusters.TPU_V5P_128_CLUSTER,
+    DAG_ID,
+)
+
+with DAG(
+    dag_id=DAG_ID,
+    start_date=datetime(2025, 1, 1),
+    schedule=SCHEDULE,
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=120),
+    tags=["timeout-test"],
+) as dag:
+
+  @task
+  def sleep_121_minutes():
+
+    print(
+      f"schedule: {SCHEDULE}",
+      "Task started, sleeping for 121 minutes...",
+    )
+    time.sleep(121 * 60)
+    print("Task finished!")
+    sleep_121_minutes()
