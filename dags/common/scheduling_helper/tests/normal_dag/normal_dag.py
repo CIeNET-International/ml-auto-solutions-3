4573f3e021e2e92cc9ce3018ee7fdb49cd487b93
Kim Pai <kim.pai@cienet.com>
2026-01-06 09:19:47 +0800

    fix: test dags lambda error

--- dags/common/scheduling_helper/tests/normal_dag/normal_dag.py
+++ dags/common/scheduling_helper/tests/normal_dag/normal_dag.py
@@ -3,8 +3,8 @@ from airflow.operators.python import PythonOperator
 import datetime as dt
 
 
-def task_fn(name):
-  print(f"Running {name}")
+def task_fn():
+  print("Running")
 
 
 with DAG(
@@ -16,7 +16,7 @@ with DAG(
 ) as dag1:
   t1 = PythonOperator(
       task_id="task1",
-      python_callable=lambda: task_fn("dag_one"),
+      python_callable=task_fn,
   )
 
 with DAG(
@@ -28,5 +28,5 @@ with DAG(
 ) as dag2:
   t2 = PythonOperator(
       task_id="task2",
-      python_callable=lambda: task_fn("dag_two"),
+      python_callable=task_fn,
   )
