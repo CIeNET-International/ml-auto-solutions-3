3b107c13815982a608856270fd9f28b56e047183
Kim Pai <kim.pai@cienet.com>
2026-01-06 09:19:47 +0800

    fix: test dags lambda error

--- dags/common/scheduling_helper/tests/no_timeout_dag/no_timeout_dag.py
+++ dags/common/scheduling_helper/tests/no_timeout_dag/no_timeout_dag.py
@@ -3,8 +3,8 @@ from airflow.operators.python import PythonOperator
 import datetime as dt
 
 
-def task_fn(name):
-  print(f"Running {name}")
+def task_fn():
+  print(f"Running")
 
 
 with DAG(
@@ -15,5 +15,5 @@ with DAG(
 ) as dag1:
   t1 = PythonOperator(
       task_id="task1",
-      python_callable=lambda: task_fn("dag_one"),
+      python_callable=task_fn,
   )
