407c1cf1adf3e93e89c15b872ee0e009be50ca4e
EmmaLien <emma.lien@cienet.com>
2026-01-20 13:13:53 +0800

    fix

--- dags/tpu_observability/utils/sdk_util.py
+++ dags/tpu_observability/utils/sdk_util.py
@@ -19,48 +19,51 @@ import textwrap
 from dags.tpu_observability.utils import jobset_util as jobset
 from dags.tpu_observability.utils import subprocess_util as subprocess
 
+
 class TpuMonitoringScript:
-    """Predefined Python scripts for TPU monitoring SDK."""
-    HELP = textwrap.dedent(
-        """
+  """Predefined Python scripts for TPU monitoring SDK."""
+
+  HELP = textwrap.dedent(
+      """
         from libtpu.sdk import tpumonitoring
         tpumonitoring.help()
         """
-    ).strip()
+  ).strip()
 
-    LIST_SUPPORTED_METRICS = textwrap.dedent(
-        """
+  LIST_SUPPORTED_METRICS = textwrap.dedent(
+      """
         from libtpu.sdk import tpumonitoring
         print(tpumonitoring.list_supported_metrics())
         """
-    ).strip()
+  ).strip()
 
-def execute_tpu_python_command(
+
+def execute_sdk_command(
     info: jobset.ClusterInfo,
     pod_name: str,
     script: str,
-    namespace: str = "default"
+    namespace: str = "default",
 ) -> str:
-    """Executes a predefined Python script inside a specific TPU pod via kubectl exec.
+  """Executes a predefined Python script inside a specific TPU pod via kubectl exec.
 
-    Args:
-        info: Node pool and cluster information.
-        pod_name: The name of the target pod.
-        script: The Python script to run (use TpuMonitoringScript options).
-        namespace: Kubernetes namespace.
+  Args:
+      info: Node pool and cluster information.
+      pod_name: The name of the target pod.
+      script: The Python script to run (use TpuMonitoringScript options).
+      namespace: Kubernetes namespace.
 
-    Returns:
-        The standard output of the executed command.
-    """
-    with tempfile.NamedTemporaryFile() as temp_config_file:
-        env = os.environ.copy()
-        env["KUBECONFIG"] = temp_config_file.name
+  Returns:
+      The standard output of the executed command.
+  """
+  with tempfile.NamedTemporaryFile() as temp_config_file:
+    env = os.environ.copy()
+    env["KUBECONFIG"] = temp_config_file.name
 
-        cmd = " && ".join([
-            jobset.Command.get_credentials_command(info),
-            (
-                f'kubectl exec {pod_name} -n {namespace} '
-                f'-- python3 -c "{script}"'
-            ),
-        ])
-        return subprocess.run_exec(cmd, env=env)
+    cmd = " && ".join([
+        jobset.Command.get_credentials_command(info),
+        (
+            f"kubectl exec {pod_name} -n {namespace} "
+            f'-- python3 -c "{script}"'
+        ),
+    ])
+    return subprocess.run_exec(cmd, env=env)
