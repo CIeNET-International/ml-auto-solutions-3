12287f43ae1bf608e5f42ac5bdd5c5f7b0a84a51
yuna-tzeng <yuna.tzeng@cienet.com>
2026-02-05 07:21:22 +0000

    fix

--- dags/tpu_observability/jobset_ttr_rollback.py
+++ dags/tpu_observability/jobset_ttr_rollback.py
@@ -128,11 +128,27 @@ with models.DAG(  # pylint: disable=unexpected-keyword-arg
           task_id="rollback_node_pool"
       )(node_pool=cluster_info)
 
-      wait_for_metric_upload = jobset.wait_for_jobset_ttr_to_be_found.override(
-          task_id="wait_for_jobset_ttr_to_be_found"
+      wait_for_recovery = jobset.wait_for_all_pods_running.override(
+          task_id="wait_for_recovery",
+          poke_interval=5
       )(
           node_pool=cluster_info,
           jobset_config=jobset_config,
+      )
+
+      verify_duration = jobset.verify_recovery_duration.override(
+          task_id="verify_recovery_duration"
+      )(
+          start_time=rollback_node_pool
+      )
+
+      wait_for_metric_upload = jobset.wait_for_jobset_ttr_to_be_found.override(
+          task_id="wait_for_jobset_ttr_to_be_found",
+          trigger_rule=TriggerRule.ALL_DONE
+      )(
+          node_pool=cluster_info,
+          jobset_config=jobset_config,
+          start_time=rollback_node_pool,
       )
 
       cleanup_workload = jobset.end_workload.override(
@@ -158,6 +174,8 @@ with models.DAG(  # pylint: disable=unexpected-keyword-arg
           start_workload,
           ensure_all_pods_running,
           rollback_node_pool,
+          wait_for_recovery,
+          verify_duration,
           wait_for_metric_upload,
           cleanup_workload,
           cleanup_node_pool,
