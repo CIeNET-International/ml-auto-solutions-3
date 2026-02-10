34f161c9d4ef13d591215d4cb6898b5354ee93a4
Chris Liao <chris.liao@cienet.com>
2026-02-10 20:25:05 +0800

    fix: add tpu_sdk_dag for create jobset

--- dags/tpu_observability/tpu_sdk_monitoring_validation_dag.py
+++ dags/tpu_observability/tpu_sdk_monitoring_validation_dag.py
@@ -148,6 +148,8 @@ with models.DAG(
 
     create_node_pool = node_pool.create.override(task_id="create_node_pool")(
         node_pool=cluster_info,
+        jobset_config=jobset_config,
+
     )
 
     apply_time = jobset.run_workload.override(task_id="run_workload")(
