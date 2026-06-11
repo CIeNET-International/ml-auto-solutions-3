c122acd9a5d82f299e47cf12b10640470439aa10
Jim1428 <jim.tseng@cienet.com>
2026-06-11 11:47:44 +0800

    refactor: Call load_yaml_from_gcs directly from gcs module

--- dags/tpu_observability/node_pool_status.py
+++ dags/tpu_observability/node_pool_status.py
@@ -34,6 +34,7 @@ from dags.tpu_observability.configs.common import (
 )
 from dags.tpu_observability.utils import node_pool_util as node_pool
 from dags.tpu_observability.utils.node_pool_util import NodeOperationSpec
+from xlml.apis import gcs
 
 DAG_ID = "gke_node_pool_status"
 DAGRUN_TIMEOUT = get_dag_timeout(DAG_ID)
@@ -88,7 +89,7 @@ with models.DAG(  # pylint: disable=unexpected-keyword-arg
       )
 
       problematic_node_pool_info = copy.deepcopy(node_pool_info)
-      yaml_config = node_pool.gcs.load_yaml_from_gcs(GCS_CONFIG_PATH)
+      yaml_config = gcs.load_yaml_from_gcs(GCS_CONFIG_PATH)
       wrong_node_loc = (
           yaml_config.get("dag", {})
           .get(DAG_ID, {})
