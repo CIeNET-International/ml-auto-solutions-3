2c1ee057f44f5b0591723a7339229a4999fda791
ryanliu <ryan.liu@cienet.com>
2026-08-12 09:29:58 +0800

    Refactor `XpkTask`
    This change refactors the `XpkTask` class structure to follow OOP principles, introducing clean inheritance subclasses for specific test sub-workflows (node interruption and name generation/quarantine). This allows `XpkTask` to maintain a clean interface and establishes a uniform polymorphic run() entry point across all tasks.

--- dags/multipod/maxtext_e2e_tpu_post_training.py
+++ dags/multipod/maxtext_e2e_tpu_post_training.py
@@ -16,9 +16,7 @@
 A DAG to run MaxText E2E TPU Post-Training tests.
 """
 import datetime
-import hashlib
 
-from click import command
 from airflow import models
 from airflow.models.param import Param
 from airflow.utils.task_group import TaskGroup
@@ -29,11 +27,6 @@ from dags.multipod.configs import gke_config
 
 # HF token retrieved from Airflow Variables for secure credential management
 HF_TOKEN = safe_get_from_variable("HF_TOKEN", None)
-
-
-def get_workload_name(model, mode, length=6):
-  hex_code = f"{mode}-{hashlib.sha256(model.encode()).hexdigest()}"
-  return hex_code[:length]
 
 
 with models.DAG(
@@ -194,6 +187,7 @@ with models.DAG(
               "export ENABLE_PATHWAYS_PERSISTENCE='1'",
           ]
 
+<<<<<<< HEAD
           with TaskGroup(group_id=f"train-{mode}-{model}") as model_group:
             command = mode_test_config["command"]
             training_cmd = (
@@ -215,6 +209,27 @@ with models.DAG(
                 use_pathways=True,
                 priority="very-high",
             )
+=======
+          command = mode_test_config["command"]
+          training_cmd = (
+              " && ".join(
+                  environment_variables + [f"{command} {run_name} true"]
+              ),
+          )
+          training_task = gke_config.get_gke_config(
+              time_out_in_min=60,
+              num_slices=1,
+              cluster=XpkClusters.TPU_V5P_128_CLUSTER,
+              test_name=f"train-{mode}-{model}",
+              run_model_cmds=training_cmd,
+              docker_image="{{ params.docker_image }}",
+              test_owner=test_owner.SURBHI_J,
+          ).run(
+              use_pathways=True,
+              skip_post_process=True,
+              priority="very-high",
+          )
+>>>>>>> e81129d7 (Refactor `XpkTask`)
 
           to_hf_flags = mode_test_config.get("to_hf_flags", "false true")
 
