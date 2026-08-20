4f8d5bb623e7e5900ba60b5f78581b8766cc6e7d
Ryan Liu <ryan.liu@cienet.com>
2026-08-20 09:35:21 +0000

    Fix the gke_config import error for the new added DAG.

--- dags/multipod/maxtext_e2e_tpu_checkpoint_conversion.py
+++ dags/multipod/maxtext_e2e_tpu_checkpoint_conversion.py
@@ -28,7 +28,7 @@ from airflow.utils.task_group import TaskGroup
 from dags.common import test_owner
 from dags.common.quarantined_tests import safe_get_from_variable
 from dags.common.vm_resource import XpkClusters
-from dags.multipod.configs import gke_config
+from dags.multipod.configs import xpk_gke_config as gke_config
 
 HF_TOKEN = safe_get_from_variable("HF_TOKEN", None)
 
